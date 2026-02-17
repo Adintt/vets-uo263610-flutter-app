32febcc47446d048a5ffaee4a3e31cf32127e007
Adan Valles <adanvallestoral@gmail.com>
2026-02-17 20:23:37 +0100

    vets-UO263610-flutter-04-Almacenando los datos de un usuario.

--- vets_uo263610_flutter_app/lib/pages/home_page.dart
+++ vets_uo263610_flutter_app/lib/pages/home_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:vets_uo263610_flutter_app/src/user.dart';
 import 'package:vets_uo263610_flutter_app/pages/user_signup_form.dart';
-
+import 'package:vets_uo263610_flutter_app/pages/custom_alert_dialog.dart';
 
 class HomePage extends StatefulWidget {
   //final String _title;
@@ -41,6 +41,25 @@ class StateHomePage extends State<HomePage> {
           Navigator.push(
             context,
             MaterialPageRoute(builder: (context) => const UserSignUpForm()),
+          ).then(
+            (newUser) => {
+              if (newUser != null)
+                {
+                  setState(() {
+                    users.add(newUser);
+                    String message =
+                        "El usuario ${newUser.name} ha sido registrado";
+                    showDialog(
+                      context: context,
+                      builder: (context) => CustomAlertDialog.create(
+                        context,
+                        'Información',
+                        message,
+                      ),
+                    );
+                  }),
+                },
+            },
           ),
         },
         tooltip: "Registrar usuario",
